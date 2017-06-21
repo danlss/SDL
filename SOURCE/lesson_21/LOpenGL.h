f25573625be9600442fd3735ad8c70835fe51cfa
danlss <lucasdlss@gmail.com>
2017-06-21 12:30:28 -0300

    Add files via upload

---
+++ SOURCE/lesson_21/LOpenGL.h
@@ -0,0 +1,11 @@
+#ifndef LOPENGL_H
+#define LOPENGL_H
+
+#include <GL/glew.h>
+#include <SDL2/SDL.h>
+#include <SDL2/SDL_opengl.h>
+#include <GL/gl.h>
+#include <GL/glu.h>
+#include <stdio.h>
+
+#endif
