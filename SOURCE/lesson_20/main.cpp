e035375c5e45af23ba39557b607996f6feb226ea
danlss <lucasdlss@gmail.com>
2017-06-21 12:27:27 -0300

    Add files via upload

---
+++ SOURCE/lesson_20/main.cpp
@@ -0,0 +1,128 @@
+#include "LOpenGL.h"
+#include "LUtil.h"
+
+SDL_Window* gWindow = NULL;
+
+//OpenGL context
+SDL_GLContext gContext;
+
+
+bool init();
+
+//Frees media and shuts down SDL
+void close();
+
+/*
+Pre Condition:
+ -A valid OpenGL context
+ -Active modelview matrix
+Post Condition:
+ -Renders the scene
+Side Effects:
+ -Clears the color buffer
+ -Swaps the front/back buffer
+*/
+
+int main( int argc, char* args[] ){
+	//Start up SDL and create window
+	if( !init() ){
+		printf( "Failed to initialize!\n" );
+	}
+	else{
+		//Main loop flag
+		bool quit = false;
+
+		 //load media
+		if( !loadMedia() ){
+			printf( "Unable to load media!\n" );
+			return 2;
+		}
+
+		//Event handler
+		SDL_Event e;
+
+		//Enable text input
+		SDL_StartTextInput();
+
+		//While application is running
+		while( !quit ){
+            while( SDL_PollEvent( &e ) != 0 ){
+                //User requests quit
+                if( e.type == SDL_QUIT ){
+					quit = true;
+                }
+            }
+            //Render quad
+            update();
+
+            render();
+
+            //Update screen
+            SDL_GL_SwapWindow( gWindow );
+		}
+		//Disable text input
+        SDL_StopTextInput();
+
+	}
+	//Free resources and close SDL
+	close();
+
+	return 0;
+
+}
+
+
+bool init(){
+	//Initialization flag
+	bool success = true;
+
+	//Initialize SDL
+	if( SDL_Init( SDL_INIT_VIDEO ) < 0 ){
+		printf( "SDL could not initialize! SDL Error: %s\n", SDL_GetError() );
+		success = false;
+	}
+	else{
+		//Use OpenGL 2.1
+		SDL_GL_SetAttribute( SDL_GL_CONTEXT_MAJOR_VERSION, 2 );
+		SDL_GL_SetAttribute( SDL_GL_CONTEXT_MINOR_VERSION, 1 );
+
+		//Create window
+		gWindow = SDL_CreateWindow( "Licao 20", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_OPENGL | SDL_WINDOW_SHOWN );
+		if( gWindow == NULL ){
+			printf( "Window could not be created! SDL Error: %s\n", SDL_GetError() );
+			success = false;
+		}
+		else{
+			//Create context
+			gContext = SDL_GL_CreateContext( gWindow );
+			if( gContext == NULL ){
+				printf( "OpenGL context could not be created! SDL Error: %s\n", SDL_GetError() );
+				success = false;
+			}
+			else{
+				//Use Vsync
+				if( SDL_GL_SetSwapInterval( 1 ) < 0 ){
+					printf( "Warning: Unable to set VSync! SDL Error: %s\n", SDL_GetError() );
+				}
+
+				//Initialize OpenGL
+				if( !initGL() ){
+					printf( "Unable to initialize OpenGL!\n" );
+					success = false;
+				}
+			}
+		}
+	}
+
+	return success;
+}
+
+void close(){
+	//Destroy window
+	SDL_DestroyWindow( gWindow );
+	gWindow = NULL;
+
+	//Quit SDL subsystems
+	SDL_Quit();
+
+}
